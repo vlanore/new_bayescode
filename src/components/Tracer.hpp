6111794f851d521ab57cff2430b56dd04c80a232
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2019-10-28 14:44:57 +0100

    Fixed tracer bug (removed unnecessary references)

--- src/components/Tracer.hpp
+++ src/components/Tracer.hpp
@@ -83,13 +83,13 @@ class Tracer {
         for (auto& f : set_from_stream) f(is);
     }
 
-    void process_declaration(std::string name, double& d) {
+    void process_declaration(std::string name, double d) {
         header_to_stream.emplace_back([name](std::ostream& os) { os << name; });
         data_to_stream.emplace_back([&d](std::ostream& os) { os << d; });
         set_from_stream.emplace_back([&d](std::istream& is) { is >> d; });
     }
 
-    void process_declaration(std::string name, int& d) {
+    void process_declaration(std::string name, int d) {
         header_to_stream.emplace_back([name](std::ostream& os) { os << name; });
         data_to_stream.emplace_back([&d](std::ostream& os) { os << d; });
         set_from_stream.emplace_back([&d](std::istream& is) { is >> d; });
