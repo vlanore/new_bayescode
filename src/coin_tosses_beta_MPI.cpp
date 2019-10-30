0f5f7cb256b849cf19b813d99c0036e076beeb0f
Louis Duchemin <louis.duchemin@etu.univ-lyon1.fr>
2019-10-30 13:04:45 +0100

    Coin toss MPI update :  - refactoring  - Alpha/Beta params now correctly backed up

--- src/coin_tosses_beta_MPI.cpp
+++ src/coin_tosses_beta_MPI.cpp
@@ -32,149 +32,160 @@ license and that you accept its terms.*/
 #include "mpi_components/gather.hpp"
 using namespace std;
 
-TOKEN(beta_weight_a)
-TOKEN(beta_weight_b)
+TOKEN(shape_a)
+TOKEN(shape_b)
 TOKEN(p)
 TOKEN(bern)
 
 // Model definition
-auto common_bernoulli_model(size_t nb_coins, size_t nb_draws) {
-    auto beta_weight_a = make_node<exponential>(1.0);
-    auto beta_weight_b = make_node<exponential>(1.0);
-    auto p = make_node_array<beta_ss>(nb_coins, n_to_one(beta_weight_a), n_to_one(beta_weight_b));
+auto coin_toss_model(size_t nb_coins, size_t nb_draws) {
+    auto shape_a = make_node<exponential>(1.0);
+    auto shape_b = make_node<exponential>(1.0);
+    auto p = make_node_array<beta_ss>(nb_coins, n_to_one(shape_a), n_to_one(shape_b));
     auto bern = make_node_matrix<bernoulli>(
         nb_coins, nb_draws, [& v = get<value>(p)](int i, int) { return v[i]; });
+
     // clang-format off
     return make_model(
-            beta_weight_a_ = move(beta_weight_a),
-            beta_weight_b_ = move(beta_weight_b),
+            shape_a_ = move(shape_a),
+            shape_b_ = move(shape_b),
             p_ = move(p),
             bern_ = move(bern)
-    );  // clang-format on
+    );
+    // clang-format on
+}
+
+template <typename Model, typename Broadcast, typename Gen>
+void simulate(Model& m, Broadcast& broadcast_ab, Partition& partition, Gen& gen) {
+    const auto rank = MPI::p->rank;
+    const bool is_master = !rank;
+    const size_t partition_size = partition.my_partition_size();
+
+    // Master sets alpha and beta parameters and broadcasts it to slaves
+    auto view_ab = make_collection(get<shape_a>(m), get<shape_b>(m));
+    if (is_master) { draw(view_ab, gen); }
+    master_to_slave(broadcast_ab);
+
+    if (!is_master) {
+        auto view_coin = make_collection(p_(m), bern_(m));
+        draw(view_coin, gen);
+        for (size_t i = 0; i < partition_size; ++i) {
+            int coin_id = partition_size * (rank - 1) + (i + 1);
+            MPI::p->message("Coin {} \t: P={}", coin_id, get<p, value>(m)[i]);
+            // MPI::p->message("K [{}] \t: {}", coin_id, vector_to_string(get<bern, value>(m)[i]));
+        }
+    }
+}
+
+template <typename Model, typename Broadcast, typename Gen>
+void initialize(Model& m, Broadcast& broadcast_ab, Gen& gen) {
+    // Initialize node states to be different from simulation
+    const bool is_master = !(MPI::p->rank);
+    if (is_master) {
+        draw(shape_a_(m), gen);
+        draw(shape_b_(m), gen);
+    }
+
+    master_to_slave(broadcast_ab);
+
+    if (!is_master) { draw(p_(m), gen); }
 }
 
 int compute(char, char**) {
     auto rank = MPI::p->rank;  // getting MPI rank
-    auto size = MPI::p->size;  // ... and size
-    const auto master = !rank;
+    auto size = MPI::p->size;  // ... and size (number of processes)
+    const auto is_master = !rank;
+
+    constexpr int n_obs{50};
+    constexpr int n_coins{80};
+    constexpr size_t nb_it{1'000'000};
+    constexpr size_t burn_in{nb_it / 10};
+    constexpr size_t sample_size{nb_it - burn_in};
     auto gen = make_generator();
-    const int n_obs = 20;
-    const int n_coins = 10;
 
     // List of indices for data partition
     // indices are partitioned into size-1 bins, starting at process 1
     IndexSet indices(n_coins);
     std::generate(indices.begin(), indices.end(), [n = 0]() mutable { return n++; });
+
     Partition partition(indices, size - 1, 1);
     MPI::p->message("Partition size : {}", partition.my_partition_size());
-    // if (!master) {
-    //     for (auto idx : partition.my_partition()) {
-    //         MPI::p->message("Partition");
-    //         MPI::p->message(idx);
-    //     }
-    // }
-    constexpr size_t nb_it{1'000'000};
-    // constexpr size_t burn_in{nb_it / 10};
 
-    // Common part of the model, shared between master and slaves
-    auto m = common_bernoulli_model(master ? n_coins : partition.my_partition_size(), n_obs);
-    // Master sets alpha and beta parameters and broadcasts it to slaves
-    auto broadcast_a_b = broadcast(get<beta_weight_a, value>(m), get<beta_weight_b, value>(m));
-    // INFO(get<beta_weight_a, value>(m));
-
-    // Simulation
-    auto v_a_b = make_collection(get<beta_weight_a>(m), get<beta_weight_b>(m));
-    if (master) { draw(v_a_b, gen); }
-    master_to_slave(broadcast_a_b);
-
-    // Slaves simulate tosses, master has nullptr
-    // std::unique_ptr<decltype(slave_bernoulli_model(n_coins, n_obs, m))> tosses_model{nullptr};
-
-    if (!master) {
-        // tosses_model = std::make_unique<decltype(slave_bernoulli_model(n_coins, n_obs, m))>(
-        // slave_bernoulli_model(n_coins, n_obs, m));
-
-        auto v = make_collection(p_(m), bern_(m));
-        draw(v, gen);
-        if (rank) {
-            for (size_t i = 0; i < partition.my_partition_size(); ++i) {
-                MPI::p->message("P for coin {} : {}", (i + 1) * rank, get<p, value>(m)[i]);
-                // MPI::p->message("K obs :");
-                // for (size_t it = 0; it < n_obs; ++it) {
-                //     MPI::p->message("--- {}", get<bern, value>(m)[i][it]);
-                // }
-            }
-        }
-    }
-    // Simulation done
-
-    // Initialize node states to be different from simulation
-    if (master) {
-        draw(v_a_b, gen);
-        MPI::p->message("Alpha sim = {}", get<beta_weight_a, value>(m));
-        MPI::p->message("Beta sim = {}", get<beta_weight_b, value>(m));
-    }
-
-    master_to_slave(broadcast_a_b);
-
-    if (!master) { draw(p_(m), gen); }
-    // Initialisation done
-
-    auto v_weight_a = make_collection(beta_weight_a_(m), p_(m));
-    auto v_weight_b = make_collection(beta_weight_b_(m), p_(m));
-
-
+    // Common part of the model, shared between is_master and slaves
+    auto m = coin_toss_model(is_master ? n_coins : partition.my_partition_size(), n_obs);
+    // MPI data sharing
+    auto broadcast_ab = broadcast(get<shape_a, value>(m), get<shape_b, value>(m));
     auto gather_p = gather(partition, get<p, value>(m));
 
-    vector<double> p_sum(master ? n_coins : 0, 0);
+
+    // Simulate model parameters and initialize them
+    simulate(m, broadcast_ab, partition, gen);
+    slave_to_master(gather_p);
+    auto simulated_p = backup(get<p>(m));
+    auto simulated_a = backup(get<shape_a>(m));
+    auto simulated_b = backup(get<shape_b>(m));
+    // Initialize model state before inference
+    initialize(m, broadcast_ab, gen);
+
+
+    auto view_ap = make_collection(shape_a_(m), p_(m));
+    auto view_bp = make_collection(shape_b_(m), p_(m));
+
+    // Parameters sum tracers used by master
+    vector<double> p_sum(is_master ? n_coins : 0, 0);
     double alpha_sum = 0, beta_sum = 0;
+
+    // K sum suff stat
+    vector<int> k_sums(partition.my_partition_size(), 0);
+    if (!is_master) {
+        for (size_t i = 0; i < partition.my_partition_size(); ++i) {
+            auto& k_sum = k_sums[i];
+            subsets::row(bern_(m), i).across_values([&k_sum](int val) { k_sum += val; });
+        }
+    }
 
     for (size_t it = 0; it < nb_it; ++it) {
         // Gather current p_ values from slaves
         slave_to_master(gather_p);
 
         // Master moves alpha, beta
-        if (master) {
-            // Save p
-            for (size_t i = 0; i < n_coins; ++i) { p_sum[i] += get<p, value>(m)[i]; }
-            // Save alpha & beta hyperparams
-            alpha_sum += get<beta_weight_a, value>(m);
-            beta_sum += get<beta_weight_b, value>(m);
+        if (is_master) {
+            if (it >= burn_in) {
+                if (it == burn_in) { MPI::p->message("Burning done !"); }
+                // Save p
+                for (size_t i = 0; i < n_coins; ++i) { p_sum[i] += get<p, value>(m)[i]; }
+                // Save alpha & beta hyperparams
+                alpha_sum += get<shape_a, value>(m);
+                beta_sum += get<shape_b, value>(m);
+            }
             // Propose moves
-            scaling_move(beta_weight_a_(m), logprob_of_blanket(v_weight_a), gen);
-            scaling_move(beta_weight_b_(m), logprob_of_blanket(v_weight_b), gen);
+            scaling_move(shape_a_(m), logprob_of_blanket(view_ap), gen);
+            scaling_move(shape_b_(m), logprob_of_blanket(view_bp), gen);
         }
-        master_to_slave(broadcast_a_b);
+        master_to_slave(broadcast_ab);
 
-
-        if (!master) {
+        if (!is_master) {
             for (size_t i = 0; i < partition.my_partition_size(); ++i) {
-                // Move k_sum
-                int k_sum = 0;
-                auto k_vec = subsets::row(bern_(m), i);
-                k_vec.across_values([&k_sum](int val) { k_sum += val; });
-                beta_ss::draw(get<p, value>(m)[i], get<beta_weight_a, value>(m) + k_sum,
-                    get<beta_weight_b, value>(m) + n_obs - k_sum, gen);
-                // if (rank == 1 && it < 100) {
-                //     MPI::p->message("K sum : {}", k_sum);
-                //     MPI::p->message("P sample : {}", get<p, value>(m)[i]);
-                //     // MPI::p->message("K sum : {}", k_sum);
-                // }
+                beta_ss::draw(get<p, value>(m)[i], get<shape_a, value>(m) + k_sums[i],
+                    get<shape_b, value>(m) + n_obs - k_sums[i], gen);
             }
         }
-        // if (it >= burn_in) { p_sum += raw_value(p_(*)); }
     }
-    // float p_mean = p_sum / float(nb_it - burn_in);
-    if (master) {
+
+
+    // Sampling results
+    if (is_master) {
+        // Compute p means
         vector<double> p_means(n_coins, 0);
         std::transform(
-            p_sum.begin(), p_sum.end(), p_means.begin(), [](double v) { return v / nb_it; });
+            p_sum.begin(), p_sum.end(), p_means.begin(), [](double v) { return v / sample_size; });
         for (size_t i = 0; i < n_coins; ++i) {
-            MPI::p->message("P mean coin {} : {}", i + 1, p_means[i]);
+            MPI::p->message(
+                "Coin {}\t: P mean = {} \t| Truth = {}", i + 1, p_means[i], simulated_p[i]);
         }
-
-        MPI::p->message("Alpha = {}", alpha_sum / nb_it);
-        MPI::p->message("Beta = {}", beta_sum / nb_it);
+        // Compute alpha and beta means
+        MPI::p->message("Alpha = {} \t| Truth = {}", alpha_sum / sample_size, simulated_a);
+        MPI::p->message("Beta = {} \t| Truth = {}", beta_sum / sample_size, simulated_b);
     }
     return 0;
 }
