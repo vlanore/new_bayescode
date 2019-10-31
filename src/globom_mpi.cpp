ee679173e8e7247d749d5797512966b7321749b1
ThibaultLatrille <thibault.latrille@univ-lyon1.fr>
2019-10-31 10:01:56 +0100

    Initial commit for Globom MPI, only a copy of Globom

---
+++ src/globom_mpi.cpp
@@ -0,0 +1,71 @@
+#include "submodels/globom_mpi_model.hpp"
+#include "traceable_collection.hpp"
+#include "mpi_components/broadcast.hpp"
+#include "mpi_components/gather.hpp"
+#include "mpi_components/unique_ptr_utils.hpp"
+#include "submodels/submodel_external_interface.hpp"
+
+int main(int argc, char* argv[]) {
+    // parsing command-line arguments
+    ChainCmdLine cmd{argc, argv, "SingleOmega", ' ', "0.1"};
+    InferenceAppArgParse args(cmd);
+    cmd.parse();
+
+    // input data
+    auto data = prepare_data(args.alignment.getValue(), args.treefile.getValue());
+
+    // random generator
+    auto gen = make_generator();
+
+    // model
+    auto model = globom::make(data, gen);
+
+    // move success stats
+    MoveStatsRegistry ms;
+
+    // move schedule
+    auto scheduler = make_move_scheduler([&gen, &model]() {
+        // move phyloprocess
+        globom::touch_matrices(model);
+        phyloprocess_(model).Move(1.0);
+
+        // move omega
+        for (int rep = 0; rep < 30; rep++) {
+            // move branch lengths
+            bl_suffstats_(model).gather();
+            branchlengths_sm::gibbs_resample(branch_lengths_(model), bl_suffstats_(model), gen);
+
+            // move omega
+            path_suffstats_(model).gather();
+            omegapath_suffstats_(model).gather();
+            omega_sm::gibbs_resample(global_omega_(model), omegapath_suffstats_(model), gen);
+
+            // move nuc rates
+            nucpath_suffstats_(model).gather();
+            nucrates_sm::move_nucrates(nuc_rates_(model), nucpath_suffstats_(model), gen, 1, 1.0);
+        }
+    });
+
+    // trace
+    int youpi = 2;
+    auto trace = make_custom_tracer(cmd.chain_name() + ".trace",  //
+        trace_entry("a", [&youpi]() { return youpi; }),           //
+        trace_entry("b", get<global_omega, omega>(model))         //
+    );
+
+    // initializing components
+    ChainDriver chain_driver{cmd.chain_name(), args.every.getValue(), args.until.getValue()};
+
+    ConsoleLogger console_logger;
+    ModelTracer chain(model, cmd.chain_name() + ".chain");
+
+    // registering components to chain driver
+    chain_driver.add(scheduler);
+    chain_driver.add(console_logger);
+    chain_driver.add(chain);
+    chain_driver.add(trace);
+    chain_driver.add(ms);
+
+    // launching chain!
+    chain_driver.go();
+}
