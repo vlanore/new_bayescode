408c43f659ffe5ea6b60f4586b27ab001d0948c2
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2019-11-05 16:51:33 +0100

    nucrates_sm.hpp now using generic mh templates

--- src/submodels/nucrates_sm.hpp
+++ src/submodels/nucrates_sm.hpp
@@ -41,86 +41,52 @@ struct nucrates_sm {
             nuc_matrix_ = std::move(nuc_matrix));
     }
 
-    template <class SubModel, class LogProb, class Update, class Gen, class Reporter = NoReport>
-    static void move_exch_rates(SubModel& model, std::vector<double> tunings,
-        LogProb logprob_children, Update update, Gen& gen, Reporter reporter = {}) {
+    // move with arbitrary logprob function (for children nodes only)
+    template <class SubModel, class LogProb, class Gen, class Reporter = NoReport>
+    static void generic_move_nucrates(SubModel& model, LogProb& nuc_logprob, Gen& gen, 
+        size_t nrep = 1, double tuning_modulator = 1.0,
+        Reporter reporter = {},
+        std::vector<double> exch_tunings = {0.1, 0.03, 0.01},
+        std::vector<double> eqfreqs_tunings = {0.1, 0.03})  {
 
-        auto& target = exch_rates_(model);
+        auto nuc_update = simple_gather(nuc_matrix_(model));
 
-        for (auto tuning : tunings) {
-            auto bkp = backup(target);
-            double logprob_before = logprob_children() + logprob(target);
-            double log_hastings = profile_move(get<value>(target), tuning, gen);
-            update();
-            double logprob_after = logprob_children() + logprob(target);
-            bool accept = decide(logprob_after - logprob_before + log_hastings, gen);
-            if (!accept) {
-                restore(target, bkp);
-                update();
-            }
-            reporter.report(accept);
-        }
-    }
-
-    template <class SubModel, class LogProb, class Update, class Gen, class Reporter = NoReport>
-    static void move_eq_freqs(SubModel& model, std::vector<double> tunings,
-        LogProb logprob_children, Update update, Gen& gen, Reporter reporter = {}) {
-
-        auto& target = eq_freq_(model);
-
-        for (auto tuning : tunings) {
-            auto bkp = backup(target);
-            double logprob_before = logprob_children() + logprob(target);
-            double log_hastings = profile_move(get<value>(target), tuning, gen);
-            update();
-            double logprob_after = logprob_children() + logprob(target);
-            bool accept = decide(logprob_after - logprob_before + log_hastings, gen);
-            if (!accept) {
-                restore(target, bkp);
-                update();
-            }
-            reporter.report(accept);
-        }
-    }
-
-    template <class SubModel, class LogProb, class Update, class Gen, class Reporter = NoReport>
-    static void move_nucrates(SubModel& model,
-        LogProb logprob_children, Update update, Gen& gen, 
-        size_t nrep = 1, double tuning = 1.0,
-        Reporter reporter = {}) {
-
-        std::vector<double> exch_tunings = {0.1, 0.03, 0.01};
-        for (auto& t : exch_tunings)   {
-            t *= tuning;
-        }
-        std::vector<double> eqfreqs_tunings = {0.1, 0.03};
-        for (auto& t : eqfreqs_tunings)    {
-            t *= tuning;
-        }
         for (size_t i = 0; i<nrep; i++) {
-            move_exch_rates(model, exch_tunings, logprob_children, update, gen);
-            move_eq_freqs(model, eqfreqs_tunings, logprob_children, update, gen);
-            /*
-            move_exch_rates(model, exch_tunings, logprob_children, update, gen, reporter("exch_rates"));
-            move_eq_freqs(model, eqfreqs_tunings, logprob_children, update, gen, reporter("eq_freqs"));
-            */
+
+            for (auto& t : exch_tunings)   {
+                auto proposal = [&t, &tuning_modulator] (auto& value, auto& gen) {return profile_move(value, t*tuning_modulator, gen);};
+                sweet_mh_move(exch_rates_(model), nuc_logprob, proposal, gen, nuc_update);
+            }
+
+            for (auto& t : eqfreqs_tunings)    {
+                auto proposal = [&t, &tuning_modulator] (auto& value, auto& gen) {return profile_move(value, t*tuning_modulator, gen);};
+                sweet_mh_move(eq_freq_(model), nuc_logprob, proposal, gen, nuc_update);
+            }
         }
     }
 
     // specialised version when we give nuc path suffstats:
-    // - the log prob of the children is given by the suffstats
-    // - we also know that the only update to do is the nuc matrix itself
     template <class SubModel, class SS, class Gen, class Reporter = NoReport>
-    static void move_nucrates(SubModel& model, SS& ss, Gen& gen, 
-        size_t nrep = 1, double tuning = 1.0,
-        Reporter reporter = {}) {
+    static void move_nucrates(SubModel& model, Proxy<SS&>& ss, Gen& gen, 
+        size_t nrep = 1, double tuning_modulator = 1.0,
+        Reporter reporter = {},
+        std::vector<double> exch_tunings = {0.1, 0.03, 0.01},
+        std::vector<double> eqfreqs_tunings = {0.1, 0.03})  {
 
-        auto logprob = [&model, &ss]() {
-            return ss.get().GetLogProb(get<nuc_matrix, value>(model));
-        };
+        auto nuc_logprob = suffstat_logprob(nuc_matrix_(model), ss);
+        auto nuc_update = simple_gather(nuc_matrix_(model));
 
-        auto update = [&model]() {gather(nuc_matrix_(model));};
+        for (size_t i = 0; i<nrep; i++) {
 
-        move_nucrates(model, logprob, update, gen, nrep, tuning, reporter);
+            for (auto& t : exch_tunings)   {
+                auto proposal = [&t, &tuning_modulator] (auto& value, auto& gen) {return profile_move(value, t*tuning_modulator, gen);};
+                sweet_mh_move(exch_rates_(model), nuc_logprob, proposal, gen, nuc_update);
+            }
+
+            for (auto& t : eqfreqs_tunings)    {
+                auto proposal = [&t, &tuning_modulator] (auto& value, auto& gen) {return profile_move(value, t*tuning_modulator, gen);};
+                sweet_mh_move(eq_freq_(model), nuc_logprob, proposal, gen, nuc_update);
+            }
+        }
     }
 };
