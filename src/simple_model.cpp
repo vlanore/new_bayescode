a552f66f72ee120d3303ca83f338e3a44c8e444f
Vincent Lanore <vincent.lanore@gmail.com>
2020-01-29 11:04:20 +0100

    Fixed simple model + added comments explaining the fix

--- src/simple_model.cpp
+++ src/simple_model.cpp
@@ -16,10 +16,12 @@ struct simple_model {
     static auto make()    {
         // array of (dynamically allocated) objects of type C
         // dynamic allocation of the vector itself (but also tried without)
-        auto v = std::make_unique<std::vector<std::unique_ptr<C>>>();
+        auto v = std::vector<std::unique_ptr<C>>();
+        // VL : no need to put the vector in a unique_ptr, as vectors already
+        // allocate their contents on the heap
 
         // fill vector with complicated objects
-        v->emplace_back(std::make_unique<C>(3));
+        v.emplace_back(std::make_unique<C>(3));
 
         // move vector into tagged tuple
         return make_model(array_ = move(v));
@@ -28,6 +30,9 @@ struct simple_model {
 
 int main()  {
     auto model = simple_model::make();
-    auto v = get<array>(model);     // error: use of deleted function
+    auto& v = get<array>(model);     // error: use of deleted function
+    // VL: "auto v = get<array>(model)" was trying to make a copy of the whole vector,
+    // which is impossible because it's a vector of unique pointers which are not copyable
+    // taking a reference instead ("auto& v = ...") solves the problem
     std::cerr << "size : " << v.size() << '\n';
 }
