8092c0c006245ab11d016afef5c6a948147cd3b6
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2021-12-24 14:52:40 +0100

    mutselomega: adding Ne parameter

--- src/submodels/mutselomega.hpp
+++ src/submodels/mutselomega.hpp
@@ -6,12 +6,13 @@
 struct mutselomega {
 
     using T = AAMutSelOmegaCodonSubMatrix;
-    using param_decl = param_decl_t<param<nucmatrix, SubMatrix>, param<freqs, std::vector<double>>, param<real_a, spos_real>>;
+    using param_decl = param_decl_t<param<nucmatrix, SubMatrix>, param<freqs, std::vector<double>>, param<real_a, spos_real>, param<real_b, spos_real>>;
 
-    static void gather(T& mat, const SubMatrix& nucmat, const std::vector<double>& aafitness, double omega)  {
+    static void gather(T& mat, const SubMatrix& nucmat, const std::vector<double>& aafitness, double omega, double Ne)  {
             mat.SetNucMatrix(&nucmat);
             mat.SetFitness(aafitness);
             mat.SetOmega(omega);
+            mat.SetNe(Ne);
             mat.CorruptMatrix();
     }
 };
