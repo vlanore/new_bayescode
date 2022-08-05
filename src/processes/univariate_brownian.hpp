338ae4acc555b1e46d87e3438e03e99dabed34f7
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2022-08-05 08:09:53 +0200

    completed the drawing methods with paths in univariate brownian

--- src/processes/univariate_brownian.hpp
+++ src/processes/univariate_brownian.hpp
@@ -35,8 +35,116 @@ struct root_mean{};
 struct root_variance{};
 struct brownian_tau{};
 
+struct normal_mean_condL    {
+
+    // conditional likelihoods
+    // normalization constant, mean and precision
+    // (logK, m, tau)
+    // L(x) = K * exp(-0.5*tau*(x-m)^2)
+
+    using L = std::tuple<double, double, double>;
+    using T = real;
+    using Constraint = bool;
+
+    static L make_init()  {
+        return std::tuple<double, double, double>(0,0,0);
+    }
+
+    static void init(L& condl)  {
+        get<0>(condl) = 0;
+        get<1>(condl) = 0;
+        get<2>(condl) = 0;
+    }
+
+    static void init(L& condl, const real& x, const bool& clamp)    {
+        if (clamp)  {
+            get<0>(condl) = 0;
+            get<1>(condl) = x;
+            get<2>(condl) = std::numeric_limits<double>::infinity();
+        }
+        else    {
+            get<0>(condl) = 0;
+            get<1>(condl) = 0;
+            get<2>(condl) = 0;
+        }
+    }
+
+    // multiply condl into res_condl
+    static void multiply(const L& condl, L& res_condl)   {
+
+        double logK1 = get<0>(condl);
+        double logK2 = get<0>(res_condl);
+
+        double m1 = get<1>(condl);
+        double m2 = get<1>(res_condl);
+
+        double tau1 = get<2>(condl);
+        double tau2 = get<2>(res_condl);
+
+        double tau = tau1 + tau2;
+        double m = (tau1*m1 + tau2*m2)/tau;
+        double taup = tau1*tau2/(tau1 + tau2);
+        double dm = m1-m2;
+
+        double logK = logK1 + logK2 + 0.5*log(taup/2/constants::pi) - 0.5*taup*dm*dm;
+
+        get<0>(res_condl) = logK;
+        get<1>(res_condl) = m;
+        get<2>(res_condl) = tau;
+    }
+
+    // vector versions
+
+    static void init(std::vector<L>& condls)  {
+        for(auto& condl : condls)   {
+            get<0>(condl) = 0;
+            get<1>(condl) = 0;
+            get<2>(condl) = 0;
+        }
+    }
+
+    static void multiply(const std::vector<L>& from_condls, std::vector<L>& to_condls)   {
+        for (size_t i=0; i<from_condls.size(); i++)  {
+            multiply(from_condls.at(i), to_condls[i]);
+        }
+    }
+
+    static void mix(L& condl, const std::vector<L>& condls)   {
+        init(condl);
+        double maxlogK = 0;
+        for (const auto& l : condls)    {
+            if (maxlogK < get<0>(l))   {
+                maxlogK = get<0>(l);
+            }
+        }
+        double K = 0;
+        std::vector<double> w(condls.size(), 0);
+        for (size_t i=0; i<condls.size(); i++)  {
+            w[i] = exp(get<0>(condls.at(i))) - maxlogK;
+            K += w[i];
+        }
+        double logK = log(K) + maxlogK;
+
+        double mu = 0;
+        double v = 0;
+        for (size_t i=0; i<condls.size(); i++)  {
+            mu += w[i] * get<1>(condls.at(i));
+            v += w[i] / get<2>(condls.at(i));
+        }
+        mu /= K;
+        v /= K;
+        double tau = 1.0 / v;
+
+        get<0>(condl) = logK;
+        get<1>(condl) = mu;
+        get<2>(condl) = tau;
+    }
+};
+
 template<class T>
 struct discretized_path : public std::vector<T>, public custom_tracer {
+
+    // dimension n+2 is: number of stochastic dfs (including starting point)
 
     discretized_path(size_t n) : std::vector<T>(n+2,T()) {}
     discretized_path(size_t n, const T& initT) : std::vector<T>(n+2, initT) {}
@@ -46,10 +154,12 @@ struct discretized_path : public std::vector<T>, public custom_tracer {
         return 1.0 / (this->size()-1);
     }
 
+    // valid for all points: interior + boundaries
     double get_midpoint_time(size_t i) const {
         return double(i)/(this->size()-1);
     }
 
+    // for all points: interior + boundaries
     double get_breakpoint_time(size_t i) const {
         if (i == 0) return 0;
         if (i == this->size()) return 1.0;
@@ -126,46 +236,6 @@ static auto get_branch_sum(const discretized_path<T>& path, double t_old, double
     return mean * (t_old-t_young);
 } 
 
-struct normal_mean_condL    {
-
-    // conditional likelihoods
-    // normalization constant, mean and precision
-    // (logK, m, tau)
-    // L(x) = K * exp(-0.5*tau*(x-m)^2)
-
-    using L = std::tuple<double, double, double>;
-    using T = real;
-    using Constraint = bool;
-
-    static L make_init()  {
-        return std::tuple<double, double, double>(0,0,0);
-    }
-
-    static void init(L& condl)  {
-        get<0>(condl) = 0;
-        get<1>(condl) = 0;
-        get<2>(condl) = 0;
-    }
-
-    static void init(L& condl, const real& x, const bool& clamp)    {
-        if (clamp)  {
-            get<0>(condl) = 0;
-            get<1>(condl) = x;
-            get<2>(condl) = std::numeric_limits<double>::infinity();
-        }
-        else    {
-            get<0>(condl) = 0;
-            get<1>(condl) = 0;
-            get<2>(condl) = 0;
-        }
-    }
-
-    // multiply condl into res_condl
-    static void multiply(const L& condl, L& res_condl)   {
-    }
-
-};
-
 struct univariate_normal    {
 
     using T = real;
@@ -186,7 +256,7 @@ struct univariate_normal    {
 
     static real logprob(T& x, real mean, spos_real variance)   {
         double y = (x - mean) / variance;
-        return -0.5 * y * y - log(variance * sqrt(2.0 * constants::pi));
+        return -0.5*log(2.0*constants::pi*variance) - 0.5*y*y;
     }
 
     template<typename Gen>
@@ -214,22 +284,66 @@ struct univariate_brownian {
 
     template <typename Gen>
     static void path_draw(pathT& path, instantT& x_young, const instantT& x_old, double t_young, double t_old, spos_real tau, Gen& gen)   {
-        std::normal_distribution<double> distrib(real(x_old), positive_real((t_old-t_young)/tau));
-        x_young = distrib(gen);
+
+        double dt = (t_old - t_young)*path.get_width();
+        double vstep = dt/tau;
+
         path[0] = x_old;
-        path[1] = x_young;
+        for (size_t i=1; i<path.size(); i++) {
+            std::normal_distribution<double> distrib(real(path[i-1]), positive_real(vstep));
+            path[i] = distrib(gen);
+        }
+        x_young = path[path.size()-1];
     }
 
+    // why x_young and x_old ? don't seem to be necessary
     static real path_logprob(pathT& path, const instantT& x_young, const instantT& x_old, double t_young, double t_old, spos_real tau)   {
-        double v = (t_old-t_young)/tau;
-        double y = (x_young - x_old) / v;
-        return -0.5 * y * y - log(v * sqrt(2.0 * constants::pi));
+
+        double s2 = 0;
+        for (size_t i=1; i<path.size(); i++) {
+            double d = path[i] - path[i-1];
+            s2 += d*d;
+        }
+
+        double dt = (t_old - t_young)*path.get_width();
+        double vstep = dt/tau;
+        return -0.5*(path.size()-1)*log(2.0*constants::pi*vstep) - 0.5*s2/vstep;
     }
 
     static void backward_propagate(const CondL::L& condl_young, CondL::L& condl_old, double t_young, double t_old, spos_real tau)  {
+
+        get<0>(condl_old) = get<0>(condl_young);
+        get<1>(condl_old) = get<1>(condl_young);
+        double v = 1.0/get<2>(condl_old) + (t_old-t_young)/tau;
+        get<2>(condl_old) = 1.0/v;
     }
 
     static void backward_propagate(const CondL::L& condl_young, const CondL::L& condl_branch, CondL::L& condl_old, double t_young, double t_old, spos_real tau)  {
+
+        double u = 1.0/(t_old - t_young);
+        double tau0 = get<2>(condl_young);
+        double taub = get<2>(condl_branch);
+
+        double m0 = get<1>(condl_young);
+        double mb = get<1>(condl_branch);
+
+        double tau1 = tau0 + u*tau + taub/4;
+        double z = tau0*u*tau + tau0*taub/4 + u*tau*taub;
+        double tau2 = z/tau1;
+        double tau3 = tau0*u*tau*taub/z;
+
+        double m2 = (tau0*u*tau*m0 + tau0*taub/4*(2*mb-m0) + u*tau*taub*mb)/z;
+
+        double v2 = (tau0*u*tau*m0*m0 + tau0*taub/4*(2*mb-m0)*(2*mb-m0) + u*tau*taub*mb*mb)/z 
+            - m2*m2;
+
+        get<0>(condl_old) = get<0>(condl_young) + get<0>(condl_branch) 
+            + 0.5*log(tau3/2/constants::pi) 
+            - 0.5*tau2*v2;
+
+        get<1>(condl_old) = m2;
+
+        get<2>(condl_old) = tau2;
     }
 
     template<typename Gen>
@@ -254,10 +368,11 @@ struct univariate_brownian {
     template<typename Gen>
     static double path_kernel(double tuning, instantT& x_young, pathT& path, 
             double t_young, double t_old, spos_real tau, Gen& gen)  {
+
+        double mean = 0;
+        double var = tuning*path.get_width()/tau;
+
         for (size_t i=1; i<path.size(); i++)    {
-            double mean = 0;
-            double rel_prec = path.get_midpoint_time(i) - path.get_midpoint_time(i-1);
-            double var = tuning/tau/rel_prec;
             std::normal_distribution<double> distrib(real(mean), positive_real(var));
             path[i] += distrib(gen);
         }
@@ -270,13 +385,15 @@ struct univariate_brownian {
     static double bridge_kernel(double tuning, pathT& path, 
             double t_young, double t_old, spos_real tau, Gen& gen)  {
 
-        const instantT& val0 = path[0];
         const instantT& val1 = path[path.size()-1];
 
         for (size_t i=1; i<path.size()-1; i++)    {
-            double mean = ((path.get_midpoint_time(i) - path.get_midpoint_time(i-1))*val1 + (1.0 - path.get_midpoint_time(i))*val0) / path.get_midpoint_time(i);
-            double rel_prec = 1.0/(path.get_midpoint_time(i) - path.get_midpoint_time(i-1)) + 1.0/(1.0 - path.get_midpoint_time(i));
-            double var = tuning/tau/rel_prec;
+
+            double mean = (path[i-1] + (path.size()-i-1)*val1) / (path.size()-i);
+
+            // check this
+            double var = tuning * path.get_width()  / (1.0 + 1.0/(path.size()-i-1));
+
             std::normal_distribution<double> distrib(real(mean), positive_real(var));
             path[i] += distrib(gen);
         }
